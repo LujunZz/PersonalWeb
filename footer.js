9012d85914e74ce255154658c3e878e24dfd392a
LujunZz <linczhang@126.com>
2020-06-02 23:54:16 -0500

    MinorUpdate

--- footer.js
+++ footer.js
@@ -1,4 +1,4 @@
-document.write('<p>Copyright &copy; 2019, Lujun Zhang (Updated 05/05/2020)<br>\
+document.write('<p>Copyright &copy; 2019, Lujun Zhang (Updated 06/02/2020)<br>\
 University of Oklahoma<br>\
 email: lujun.zhjang @ ou.edu<p>\
 ')
